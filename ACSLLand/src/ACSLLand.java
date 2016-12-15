36b0a3b8e8576a75171325adcbd6164dc5e38be3
hermanpeng20000 <hermanpeng2000@gmail.com>
2016-12-15 08:01:34 -0800

    12/15/16

---
+++ ACSLLand/src/ACSLLand.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+/* Herman Peng, Ahmed Alsabbah, Michael Chang
+ * 1st period
+ * December 13th, 2016
+ * ACCSLLand Project
+ */
+
+public class ACSLLand {
+	
+	public static void main(String[] args) {
+		
+		
+		for (int i = 0; i < 5; i++) {
+			Scanner diceRoll = new Scanner(System.in);
+			int roll = diceRoll.nextInt();
+			while (roll != 0) {
+				int counter = 0;
+				if (counter % 2 == 0) {
+					playerA; //add stuff later for showing playerA has turn
+				} else {
+					playerB; //add stuff later for showing playerB has turn
+				}
+				counter++;
+			}
+		}
+	}
+	
+	public static 
+}
